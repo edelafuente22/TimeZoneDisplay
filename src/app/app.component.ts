ef60980c1b980e5effab8b9c2de0e5ba87826323
edelafuente22 <ofthefountain22@gmail.com>
2018-01-10 14:44:33 -0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  time = new Date();
+  timeZone = null;
+
+  selectZone(timezone) {
+    this.time = new Date();
+    if (timezone === 'MST') {
+      this.time.setHours(this.time.getHours() + 1);
+    } else if (timezone === 'CST') {
+      this.time.setHours(this.time.getHours() + 2);
+    } else if (timezone === 'EST') {
+      this.time.setHours(this.time.getHours() + 3);
+    } else if (timezone === 'clear') {
+      this.time = null;
+    }
+    
+    this.timeZone = timezone;
+  }
+}
+
+
